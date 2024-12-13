4d3ce5ce1452402fdb4ba91ad885797444199da7
joselr <joselr@vbox.home>
2024-12-13 12:53:25 +0100

    Cambios finales

--- docs/main.tf
+++ docs/main.tf
@@ -10,77 +10,86 @@ terraform {
 provider "docker" {}
 
 resource "docker_network" "jenkins" {
-  name = "jenkins-network"
+  name = "jenkins"
 }
 
-resource "docker_volume" "docker_certs" {
+resource "docker_volume" "jenkins_data" {
+  name = "jenkins-data"
+}
+
+resource "docker_volume" "jenkins_docker_certs" {
   name = "jenkins-docker-certs"
 }
 
+resource "docker_container" "docker_in_docker" {
+  name    = "jenkins-docker"
+  image   = "docker:dind"
+  restart = "no"
 
-
-resource "docker_container" "dind" {
-  name  = "jenkins-docker"
-  image = "docker:dind"
   privileged = true
-
-  env = [
-    "DOCKER_TLS_CERTDIR=/certs",
-    "DOCKER_CERT_PATH=/certs/client",
-    "DOCKER_HOST=tcp://0.0.0.0:2376",
-    "DOCKER_TLS_VERIFY=1"
-  ]
 
   networks_advanced {
     name = docker_network.jenkins.name
     aliases = ["docker"]
   }
 
-  volumes {
-    volume_name    = "jenkins-docker-certs"
-    container_path = "/certs"
-  }
+  env = [
+    "DOCKER_TLS_CERTDIR=/certs"
+  ]
 
   ports {
     internal = 2376
     external = 2376
   }
+
+  volumes {
+    volume_name    = docker_volume.jenkins_docker_certs.name
+    container_path = "/certs/client"
+  }
+
+  volumes {
+    volume_name    = docker_volume.jenkins_data.name
+    container_path = "/var/jenkins_home"
+  }
+
+  command = ["--storage-driver", "overlay2"]
 }
 
-
-
 resource "docker_container" "jenkins" {
-  name  = "jenkins-blueocean"
-  image = "myjenkins-blueocean"
+  name    = "jenkins-blueocean"
+  image   = "myjenkins-blueocean"
   restart = "on-failure"
-
-  env = [
-    "DOCKER_HOST=tcp://jenkins-docker:2376",
-    "DOCKER_CERT_PATH=/certs/client",
-    "DOCKER_TLS_VERIFY=1"
-  ]
 
   networks_advanced {
     name = docker_network.jenkins.name
   }
+
+  env = [
+    "DOCKER_HOST=tcp://docker:2376",
+    "DOCKER_CERT_PATH=/certs/client",
+    "DOCKER_TLS_VERIFY=1"
+  ]
 
   ports {
     internal = 8080
     external = 8080
   }
 
+  ports {
+    internal = 50000
+    external = 50000
+  }
+
   volumes {
-    volume_name    = "jenkins-data"
+    volume_name    = docker_volume.jenkins_data.name
     container_path = "/var/jenkins_home"
   }
 
-    volumes {
-    volume_name    = "jenkins-docker-certs"
+  volumes {
+    volume_name    = docker_volume.jenkins_docker_certs.name
     container_path = "/certs/client"
+    read_only      = true
   }
-
-
-  depends_on = [docker_container.dind]
 }
 
 
